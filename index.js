ea1f4a8f318fd8d60279a01a739a5d13d7b0d031
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2023-12-30 00:48:28 +0100

    Feat: Finished console game

--- index.js
+++ index.js
@@ -1,131 +1,162 @@
-// Game IIFE
-const game = (() => {
-  const size = 3;
-  const board = [];
+const createGameBoard = () => {
+  const board = ["", "", "", "", "", "", "", "", ""];
+  return board;
+};
 
-  for (let i = 0; i < size; i++) {
-    board[i] = [];
-    for (let j = 0; j < size; j++) {
-      board[i].push("");
+const Gameboard = createGameBoard();
+
+Gameboard.setCell = (pos) => {
+  Game().getCurrentPlayer().token === "X" ? (token = "X") : (token = "O");
+  Game().currentPlayer = Game().getCurrentPlayer();
+  let tokenIsSet = false;
+  if (Gameboard[pos] === "") {
+    Gameboard[pos] = token;
+    tokenIsSet = true;
+    displayController().showsGameBoard();
+    console.log(
+      `The player "${Game().getCurrentPlayer().name}" draw "${
+        Game().getCurrentPlayer().token
+      }" in the position ${pos}`
+    );
+    // Gameboard.checkForDraw() only for test
+    Gameboard.checkForWin(Game().getCurrentPlayer().token);
+    if (
+      Gameboard.checkForDraw() === false &&
+      Gameboard.checkForWin(Game().getCurrentPlayer().token) === false
+    ) {
+      displayController().showsTie();
+      Gameboard.reset();
+      displayController().showsGameBoard();
+    }
+    if (
+      tokenIsSet &&
+      Gameboard.checkForWin(Game().getCurrentPlayer().token) === false &&
+      Gameboard.checkForDraw() === true
+    ) {
+      //If there is no tie or winner, change turn
+      Game().switchPlayerTurn();
+      displayController().showsTurn();
+    }
+    return tokenIsSet;
+  } else {
+    console.log(`The position "${pos}" was occupied. Try again!`);
+    return;
+  }
+};
+
+Gameboard.reset = () => {
+  for (let i = 0; i < Gameboard.length; i++) {
+    Gameboard[i] = "";
+  }
+  return true;
+};
+
+Gameboard.checkForWin = (token) => {
+  if (
+    (Gameboard[0] === token &&
+      Gameboard[1] === token &&
+      Gameboard[2] === token) ||
+    (Gameboard[3] === token &&
+      Gameboard[4] === token &&
+      Gameboard[5] === token) ||
+    (Gameboard[6] === token &&
+      Gameboard[7] === token &&
+      Gameboard[8] === token) ||
+    (Gameboard[0] === token &&
+      Gameboard[3] === token &&
+      Gameboard[6] === token) ||
+    (Gameboard[1] === token &&
+      Gameboard[4] === token &&
+      Gameboard[7] === token) ||
+    (Gameboard[2] === token &&
+      Gameboard[5] === token &&
+      Gameboard[8] === token) ||
+    (Gameboard[0] === token &&
+      Gameboard[4] === token &&
+      Gameboard[8] === token) ||
+    (Gameboard[0] === token &&
+      Gameboard[1] === token &&
+      Gameboard[2] === token) ||
+    (Gameboard[2] === token && Gameboard[4] === token && Gameboard[6] === token)
+  ) {
+    displayController().showsWinner();
+    Gameboard.reset();
+    displayController().showsGameBoard();
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Gameboard.checkForDraw = () => {
+  // If there are no moves available it returns false otherwise it returns true
+  for (let i = 0; i < Gameboard.length; i++) {
+    if (Gameboard.indexOf("") !== -1) {
+      // console.log(true); Only for test
+      return true; // Movements are available
+    } else {
+      // console.log(false); Only for test
+      return false; //No moves available
     }
   }
-  const Gameboard = { board: board };
-  // Factory
-  const getBoard = () => Gameboard.board;
+};
 
-  return { getBoard };
-})();
+const createPlayer = (name, token) => {
+  return { name, token };
+};
+const playerOne = createPlayer("Player One", "X");
+const playerTwo = createPlayer(
+  "Mr. Robot",
+  playerOne.token === "X" ? "O" : "X"
+);
+const players = [playerOne, playerTwo];
 
-// Player IIFE
-const player = (() => {
-  // Factory
-  const createPlayer = (name, token) => {
-    return { name, token };
+let currentPlayer = players[0];
+
+const displayController = () => {
+  const startMessage = () => {
+    console.log("The play Tic Tac Toe Start!!");
+  };
+  const showsGameBoard = () => {
+    console.log(`| ${Gameboard[0]} | ${Gameboard[1]} | ${Gameboard[2]} |`);
+    console.log(`| ${Gameboard[3]} | ${Gameboard[4]} | ${Gameboard[5]} |`);
+    console.log(`| ${Gameboard[6]} | ${Gameboard[7]} | ${Gameboard[8]} |`);
   };
 
-  return {
-    createPlayer,
-  };
-})();
-
-// Controller IIFE
-const controller = (() => {
-  const playerOne = player.createPlayer("Player One", "X");
-  const playerTwo = player.createPlayer(
-    "Mr. Robot",
-    playerOne.token === "X" ? "O" : "X"
-  );
-
-  let actualPlayer = playerOne;
-
-  const switchPlayer = () => {
-    // Switch between Players
-    actualPlayer === playerOne
-      ? (actualPlayer = playerTwo)
-      : (actualPlayer = playerOne);
-    return actualPlayer;
+  const showsWinner = () => {
+    console.log(
+      `Tic Tac Toe! player "${Game().getCurrentPlayer().name}" wins!`
+    );
+    displayController().startMessage();
   };
 
-  const getActualPlayer = () => actualPlayer; // Show actual player
+  const showsTie = () => {
+    console.log("The Game Was Tied!!");
+    displayController().startMessage();
+  };
 
-  const putToken = (player, row, col) => {
-    let board = game.getBoard();
+  const showsTurn = () => {
+    console.log(
+      `Now is the turn of: "${Game().getCurrentPlayer().name}" with token "${
+        Game().getCurrentPlayer().token
+      }"`
+    );
+  };
 
-    board[row][col] = player.token;
-    console.log(player);
+  return { startMessage, showsGameBoard, showsWinner, showsTurn, showsTie };
+};
+
+const Game = () => {
+  const switchPlayerTurn = () => {
+    currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
   };
-  const playRound = (player, row, col) => {
-    if (game.getBoard()[row][col] === "") {
-      controller.putToken(player, row, col);
-      console.log(
-        `The Player: "${player.name}" put "${player.token}" on coordenates [${row}] [${col}]`
-      );
-      console.log(game.getBoard());
-      player = controller.switchPlayer();
-      controller.checkWinner();
-    } else {
-      console.log(`Try other coordenates. It's was ocupped!`);
-      console.log(`Turn is of ${controller.getActualPlayer().name}`);
-      return;
-    }
-    console.log(`Now turn is of the Player: ${player.name}`);
-  };
-  const checkWinner = () => {
-    let board = game.getBoard();
-    // X
-    if (
-      (board[0][0] === "X" &&
-        board[0][0] === board[0][1] &&
-        board[0][1] === board[0][2]) ||
-      (board[1][0] === "X" &&
-        board[1][0] === board[1][1] &&
-        board[1][1] === board[1][2]) ||
-      (board[2][0] === "X" &&
-        board[2][0] === board[2][1] &&
-        board[2][1] === board[2][2]) ||
-      (board[0][0] === "X" &&
-        board[0][0] === board[1][1] &&
-        board[1][1] === board[2][2]) ||
-      (board[0][2] === "X" &&
-        board[0][2] === board[1][1] &&
-        board[1][1] === board[2][0]) ||
-      (board[0][0] === "X" &&
-        board[0][0] === board[1][0] &&
-        board[1][0] === board[2][0]) ||
-      (board[0][1] === "X" &&
-        board[0][1] === board[1][1] &&
-        board[1][1] === board[2][1]) ||
-      (board[0][2] === "X" &&
-        board[0][2] === board[1][2] &&
-        board[1][2] === board[2][2]) ||
-      (board[0][0] === "O" &&
-        board[0][0] === board[0][1] &&
-        board[0][1] === board[0][2]) ||
-      (board[1][0] === "O" &&
-        board[1][0] === board[1][1] &&
-        board[1][1] === board[1][2]) ||
-      (board[2][0] === "O" &&
-        board[2][0] === board[2][1] &&
-        board[2][1] === board[2][2]) ||
-      (board[0][0] === "O" &&
-        board[0][0] === board[1][1] &&
-        board[1][1] === board[2][2]) ||
-      (board[0][2] === "O" &&
-        board[0][2] === board[1][1] &&
-        board[1][1] === board[2][0]) ||
-      (board[0][0] === "O" &&
-        board[0][0] === board[1][0] &&
-        board[1][0] === board[2][0]) ||
-      (board[0][1] === "O" &&
-        board[0][1] === board[1][1] &&
-        board[1][1] === board[2][1]) ||
-      (board[0][2] === "O" &&
-        board[0][2] === board[1][2] &&
-        board[1][2] === board[2][2])
-    ) {
-      controller.switchPlayer();
-      console.log(`${controller.getActualPlayer().name} Wins!`);
-    }
-  };
-  return { switchPlayer, getActualPlayer, putToken, playRound, checkWinner };
+  const getCurrentPlayer = () => currentPlayer;
+
+  return { switchPlayerTurn, getCurrentPlayer, currentPlayer };
+};
+
+(() => {
+  displayController().startMessage();
+  displayController().showsGameBoard();
+  displayController().showsTurn();
 })();
